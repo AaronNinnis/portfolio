df9cc1a156a39088de2d3771ae2c0c293e0786ba
AaronNinnis <aaronninnis@gmail.com>
2026-02-03 17:11:35 +1100

    Added footer and header functionality with js implementation

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,30 @@
-console.log("JS is linked");
+//console.log("JS is linked");
+
+// Wait for the entire page to load first
+document.addEventListener("DOMContentLoaded", async function() {
+    console.log("page loaded, loading footer and header");
+
+    // Load header and footer
+    await loadComponent('header', '../public/everyPage/header.html');
+    await loadComponent('footer', '../public/everyPage/footer.html');
+});
+
+// Loads an HTML file and puts it into the page
+async function loadComponent(elementId, filePath) {
+    try {
+        // Fetch HTML file
+        const response = await fetch(filePath);
+
+        // Convert to text
+        const html = await response.text();
+
+        // Find Element with same ID
+        const element = document.getElementById(elementId);
+        if (element) {
+            element.innerHTML = html;
+            console.log(`${elementId} loaded successfully.`);
+        }
+    }   catch (error) {
+        console.error(`Error loading ${elementId}:`, error);
+    }
+}
